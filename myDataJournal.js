ce224b5111abe391fed2f0165c4cae7632db1844
Gabriella Martinez <gabimayern@gmail.com>
2026-07-04 15:08:43 -0700

    Created myDataJournal.js and added days

---
+++ myDataJournal.js
@@ -0,0 +1,10 @@
+let weekData = [
+    { day: "Sunday", sleepHours: 8, screenTime: 9, mood: "Excited", caffeineIntake: 0, focusLevel: 4},
+    { day: "Monday", sleepHours: 9, screenTime: 10, mood: "Intrigued", caffeineIntake: 0, focusLevel: 7},
+    { day: "Tuesday", sleepHours: 8, screenTime: 6, mood: "Proud", caffeineIntake: 0, focusLevel: 6},
+    { day: "Wednesday", sleepHours: 7, screenTime: 8, mood: "Neutral", caffeineIntake: 1, focusLevel: 8},
+    { day: "Thursday", sleepHours: 6, screenTime: 5, mood: "Exhausted", caffeineIntake: 0, focusLevel: 5},
+    { day: "Friday", sleepHours: 9, screenTime: 12, mood: "Anxious", caffeineIntake: 0, focusLevel: 4},
+    { day: "Saturday", sleepHours: 8, screenTime: 5, mood: "Bored", caffeineIntake: 0, focusLevel: 6},
+]
+
