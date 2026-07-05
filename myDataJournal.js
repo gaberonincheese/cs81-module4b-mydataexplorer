9c6695454a22f422e9aa42d2b3299dc43847bffb
Gabriella Martinez <gabimayern@gmail.com>
2026-07-05 11:15:39 -0700

    Wrote Functions to Parse Data

--- myDataJournal.js
+++ myDataJournal.js
@@ -1,10 +1,84 @@
 let weekData = [
     { day: "Sunday", sleepHours: 8, screenTime: 9, mood: "Excited", caffeineIntake: 0, focusLevel: 4},
     { day: "Monday", sleepHours: 9, screenTime: 10, mood: "Intrigued", caffeineIntake: 0, focusLevel: 7},
-    { day: "Tuesday", sleepHours: 8, screenTime: 6, mood: "Proud", caffeineIntake: 0, focusLevel: 6},
-    { day: "Wednesday", sleepHours: 7, screenTime: 8, mood: "Neutral", caffeineIntake: 1, focusLevel: 8},
+    { day: "Tuesday", sleepHours: 8, screenTime: 6, mood: "Happy", caffeineIntake: 0, focusLevel: 8},
+    { day: "Wednesday", sleepHours: 7, screenTime: 8, mood: "Happy", caffeineIntake: 1, focusLevel: 6},
     { day: "Thursday", sleepHours: 6, screenTime: 5, mood: "Exhausted", caffeineIntake: 0, focusLevel: 5},
     { day: "Friday", sleepHours: 9, screenTime: 12, mood: "Anxious", caffeineIntake: 0, focusLevel: 4},
     { day: "Saturday", sleepHours: 8, screenTime: 5, mood: "Bored", caffeineIntake: 0, focusLevel: 6},
 ]
 
+/** Predictions
+ *  Friday was the day that had the most screen time.
+ *  Wednesday was my best focus day.
+ *  I don't drink enough caffeine to say whether or not if affects my focus.
+*/
+
+function findHighestScreenTime(dayData) {
+    let highestScreenTime = 0;
+    let dayWithHighestScreenTime = "";
+
+    for (let individualDay of dayData) {
+        if (individualDay.screenTime > highestScreenTime) {
+            highestScreenTime = individualDay.screenTime;
+            dayWithHighestScreenTime = individualDay.day;
+        } 
+    } 
+    
+    return dayWithHighestScreenTime;
+}
+
+function averageSleepHours(dayData) {
+    let totalSleepHours = 0;
+
+    for (let individualDay of dayData) {
+        totalSleepHours += individualDay.sleepHours;
+    }
+
+    return totalSleepHours / dayData.length;
+}
+
+function dayWithLeastSleep(dayData) {
+    let leastSleepHours = 20;
+    let dayWithLeastSleep = ""; 
+
+    for (let individualDay of dayData) {
+        if (individualDay.sleepHours < leastSleepHours) {
+            leastSleepHours = individualDay.sleepHours;
+            dayWithLeastSleep = individualDay.day;
+        }
+    }
+     
+    return dayWithLeastSleep;
+}
+
+function mostFrequentMood(dayData) {
+    let moodCount = {};
+
+    for (let individualDay of dayData) {
+        let mood = individualDay.mood;
+
+        if (!moodCount[mood]) {
+            moodCount[mood] = 1;
+        } else {
+            moodCount[mood]++;
+        }
+    }
+
+    let mostFrequentMood = "";
+    let highestMoodCount = 0;
+
+    for (let moodType in moodCount) {
+        if (moodCount[moodType] > highestMoodCount) {
+            highestMoodCount = moodCount[moodType];
+            mostFrequentMood = moodType;
+        }
+    }
+
+    return mostFrequentMood;
+}
+
+console.log("The day with the highest screen time is: " + findHighestScreenTime(weekData));
+console.log("The average sleep hours for the week is: " + averageSleepHours(weekData));
+console.log("The day with the least sleep hours is: " + dayWithLeastSleep(weekData));
+console.log("The most frequent mood for the week is: " + mostFrequentMood(weekData));
